10f9408391d4a01dbc339cdbb401d0b89714c5b4
Bryan Oliver <boliver@linux.com>
2022-08-04 12:49:42 -0400

    fix test script for using .tpl

--- scripts/vpc.test.sh
+++ scripts/vpc.test.sh
@@ -2,8 +2,9 @@
 export NETWORK=$1
 export ENV_PATH=./environments/$1
 
-export TF_VAR_gcp_project_id=$(cat ${ENV_PATH}.auto.tfvars.json | jq -r .project_id)
-export TF_VAR_gcp_network_name=$(cat ${ENV_PATH}.auto.tfvars.json | jq -r .network_name)
-export TF_VAR_gcp_subnets=$(cat ${ENV_PATH}.auto.tfvars.json | jq -r .subnets)
+export TF_VAR_gcp_project_id=$(cat ${ENV_PATH}.auto.tfvars.json.tpl | jq -r .project_id)
+export TF_VAR_gcp_network_name=$(cat ${ENV_PATH}.auto.tfvars.json.tpl | jq -r .network_name)
+export TF_VAR_gcp_subnets=$(cat ${ENV_PATH}.auto.tfvars.json.tpl | jq -r .subnets)
 
-echo $NETWORK $GCP_PROJECT_ID $GCP_NETWORK_NAME
+echo "PROJECT & VPC NAME" $TF_VAR_gcp_project_id $TF_VAR_gcp_network_name
+echo "SUBNETS:" $TF_VAR_gcp_subnets
